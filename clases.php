fbd138a1f241661ec14b094da23c74d6a8c195d1
Cristhian Custodio <cryisthian_06@hotmail.com>
2023-08-04 22:12:15 -0500

    clase practica de interfaces

---
+++ clases.php
@@ -0,0 +1,65 @@
+<?php 
+/**
+ * Patrom de diseño estrategia
+ */
+interface OperationInterface{
+
+    public function doOperation(float $a, float $b);
+
+}
+
+class AdditionStrategy implements OperationInterface{
+
+    public function doOperation(float $a, float $b)
+    {
+        return $a + $b;    
+    }
+}
+
+class SubstractionStrategy implements OperationInterface{
+    public function doOperation(float $a, float $b) : float|int
+    {
+        return $a - $b;
+    }
+}
+class MultiplicactionStrategy implements OperationInterface{
+    public function doOperation(float $a, float $b) : float|int
+    {
+        return $a * $b;
+    }
+}
+
+class Calculator 
+{
+    protected $operation;
+
+    
+    public function __construct(OperationInterface $operation = null)
+    {
+        $this->operation = $operation ?? new AdditionStrategy();
+
+    }
+    
+    public function execute(float $a, float $b){
+        return $this->operation->doOperation($a,$b);
+    }
+
+    public function setOperation(OperationInterface $operation)
+    {
+        $this->operation = $operation;
+    }
+     
+}
+
+$calcula = new Calculator(new MultiplicactionStrategy());
+$resultado = $calcula->execute(59,0);
+echo $resultado;
+
+
+
+
+
+
+
+
+?>
