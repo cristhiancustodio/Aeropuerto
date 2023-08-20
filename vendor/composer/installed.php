183a99fbb6cad313ed27d87b4fc62bcacaec327d
Cristhian Custodio <cryisthian_06@hotmail.com>
2023-08-19 21:37:59 -0500

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aeropuerto/slug',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f603627e867282e63d1aa53c99cca656c0d70fce',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aeropuerto/slug' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f603627e867282e63d1aa53c99cca656c0d70fce',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
